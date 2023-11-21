062aed7a9f7aa25324e8abc909b7306b36b79c39
slooi <s.looi2@outlook.com>
2023-11-21 16:53:13 +0000

    remove unnecessary consolelogs

--- src/server/api/v1/1_controllers/controller_getImageTags.ts
+++ src/server/api/v1/1_controllers/controller_getImageTags.ts
@@ -2,14 +2,10 @@ import { Request, Response } from "express"
 import { getTagsFromImageUrl } from "../2_components/service_getTagsFromImageUrl"
 
 const controller_getImageTags = async (req:Request,res:Response)=>{
-	console.log("hi sussy")
 	const tags = await getTagsFromImageUrl()
-	console.log("tags")
 	console.log("tags")
 	console.log(tags)
 	res.json(tags)
-	// console.log(typeof a)
-	// res.json(a)
 }
 
 export {controller_getImageTags}
