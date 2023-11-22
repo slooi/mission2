2d130e5704a430a8971bd594dd97636085513599
slooi <s.looi2@outlook.com>
2023-11-22 11:49:01 +0000

    Remove unnecessary console logs

--- src/server/api/v1/1_controllers/controller_getImageTags.ts
+++ src/server/api/v1/1_controllers/controller_getImageTags.ts
@@ -2,11 +2,9 @@ import { Request, Response } from "express"
 import { getTagsFromImageUrl } from "../2_components/service_getTagsFromImageUrl"
 
 const controller_getImageTags = async (req:Request,res:Response)=>{
-	const imageUrl = 'https://images-ext-2.discordapp.net/external/B7IMUnRH_Ov0yswFCdnOBqYw6TDrYtU_tMlFosEdV_M/https/pbs.twimg.com/media/F_aOvDDaIAAsXuA.jpg?width=593&height=639';
+	const imageUrl = 'https://i.pinimg.com/736x/23/f0/df/23f0df9cfe3275fe9c628cd36f09cbc6.jpg';
 
 	const tags = await getTagsFromImageUrl(imageUrl)
-	console.log("tags")
-	console.log(tags)
 	res.json(tags)
 }
 
